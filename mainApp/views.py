c2972046fb0ba7619e32f17e914b054434986270
Joanna Chien <joannachou713@gmail.com>
2020-12-17 23:06:38 +0800

    add base.html

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+import os
+
+def template_test(request):
+    return render(request, 'example.html')
